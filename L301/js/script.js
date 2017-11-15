22c7a8cba2bda620f6bca79ccfa480a85cdeda65
Dvalii1 <dvalidito1@gmail.com>
2017-11-15 20:26:07 +0400

    L301

---
+++ L301/js/script.js
@@ -0,0 +1,10 @@
+var shecdoma = ["რეკლამა", "მარკეტინგი", "ვირუსი"];
+
+var sheyvana = prompt("შეიყვანეთ სტრიქონი: ");
+
+if (sheyvana == shecdoma[0] || sheyvana == shecdoma[1] || sheyvana == shecdoma[2]){
+    alert("შეცდომაა!");
+}
+else {
+    alert("სწორია");
+}
